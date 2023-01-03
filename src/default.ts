1518e22c699be674ce86cb298bb48a340b9309c0
Michael Di Prisco <cadienvan@gmail.com>
2023-01-03 17:48:39 +0100

    chore: linting + removed example file

--- src/default.ts
+++ src/default.ts
@@ -1,3 +1,3 @@
 export const defaultConfig = {
-  amountOfTimesToCall: -1,
-}
+  amountOfTimesToCall: -1
+};
--- src/ex.ts
+++
@@ -1,8 +0,0 @@
-import { forceReturn } from './index';
-
-// Create a function which always returns 1
-const alwaysTwo = forceReturn(() => 2, 1, { amountOfTimesToCall: 1 });
-
-console.log(alwaysTwo());
-console.log(alwaysTwo());
-console.log(alwaysTwo());
