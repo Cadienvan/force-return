c2250111187f5c9d066fbf28205cecd9565c0a9b
Michael Di Prisco <cadienvan@gmail.com>
2023-01-04 21:43:07 +0100

    refactor: internal refactoring and better naming

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@
 // and returns a function which will always return the return value
 
 import { defaultConfig } from './default';
-import { Config } from './models';
+import { Config, Fn } from './models';
 
 export function forceReturn<FnArgs, ReturnValue>(
-  fn: (...args: FnArgs[]) => ReturnValue,
+  fn: Fn<FnArgs, ReturnValue>,
   returnValue: ReturnValue,
   _config: Partial<Config> = {}
 ) {
@@ -14,15 +14,15 @@ export function forceReturn<FnArgs, ReturnValue>(
     ..._config
   };
   const returnIndefinitely = config.amountOfTimesToCall === -1;
-  let amountOfTimesToCallLeft = config.amountOfTimesToCall;
+  let amountOfTimesToCallLeft: number | never = config.amountOfTimesToCall;
   // Save the original function and bring it back after config.amountOfTimesToCall calls
   const originalFn = fn;
-  let newFn = (...args: FnArgs[]) => returnValue;
+  let forcedFn = (...args: FnArgs[]) => returnValue;
   if (!returnIndefinitely)
-    newFn = (...args: FnArgs[]) => {
+    return (...args: FnArgs[]) => {
       // If the amount of times to call is 0, bring back the original function
       if (amountOfTimesToCallLeft === 0) {
-        newFn = originalFn;
+        forcedFn = originalFn;
         return originalFn(...args);
       }
       // Decrement the amount of times to call
@@ -32,5 +32,5 @@ export function forceReturn<FnArgs, ReturnValue>(
     };
 
   // Return the new function
-  return newFn;
+  return forcedFn;
 }
--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,5 @@
 export type Config = {
   amountOfTimesToCall: number;
 };
+
+export type Fn<FnArgs, ReturnValue> = (...args: FnArgs[]) => ReturnValue;
